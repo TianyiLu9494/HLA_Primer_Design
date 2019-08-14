853e444fb54fcd666f48529eb0d12c534752f2de
Tianyi Lu <tianyi9494@gmail.com>
2019-08-14 19:07:30 +1000

    unknow changes

--- shell_code/tree_construct.py
+++ shell_code/tree_construct.py
@@ -1,2 +1,6 @@
+from Bio import AlignIO #import alignIO to read alignment
+from Bio.Phylo.TreeConstruction import DistanceCalculator #DistanceCalculator can calculate the distan matrix.
 
-:q
+def Main():
+    
+    
