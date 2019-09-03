fdb0282541d0f27f94f1e28a1fa57ae3726faa4d
Tianyi Lu <tianyi9494@gmail.com>
2019-09-03 21:24:40 +1000

    arranged the write cluster sequences part into function

--- HLA_clustering/cdhit_clstr_read.py
+++ HLA_clustering/cdhit_clstr_read.py
@@ -23,27 +23,25 @@ def read_clstr(inf):
     print("{} clusters has been read.".format(cls_nb))
     return clstr
 
-
-from Bio import SeqIO
-import os
-clstr = read_clstr("A_EX2_c0.95.fasta.clstr")
-out_dic = {}
-count = 0
-rc = 0
-with open("../A_exon2_nr.fasta","r") as f:
-    for key in clstr.keys():
-        out_dic[key] = []
-    for rec in SeqIO.parse(f,"fasta"):
-        rc += 1
-        rec_name =  rec.name[-8:]
+def write_clstr(clstr):
+    from Bio import SeqIO
+    out_dic = {}
+    count = 0
+    rc = 0
+    with open("../A_exon2_nr.fasta","r") as f:
         for key in clstr.keys():
-            if rec_name in clstr[key]:
-                out_dic[key].append(rec)
-                count += 1
-print(rc)
-print(count)
-for key in out_dic.keys():
-    out_file_name = "../A_exon2_nr.fasta".replace("nr.fasta", key+".fasta")
-    with open(out_file_name,"w") as outfile:
-        SeqIO.write(out_dic[key], outfile, "fasta")
+            out_dic[key] = []
+        for rec in SeqIO.parse(f,"fasta"):
+            rc += 1
+            rec_name =  rec.name[-8:]
+            for key in clstr.keys():
+                if rec_name in clstr[key]:
+                    out_dic[key].append(rec)
+                    count += 1
+    print("{} sequences has benn parsed in".format(rc))
+    print("{} sequences will be written".format(count))
+    for key in out_dic.keys():
+        out_file_name = "../A_exon2_nr.fasta".replace("nr.fasta", key+".fasta")
+        with open(out_file_name,"w") as outfile:
+            SeqIO.write(out_dic[key], outfile, "fasta")
 
